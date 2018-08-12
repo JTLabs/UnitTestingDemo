35a65239309beff95c04f0ded5fd0ebe2865763d
Jatin Singla <jatin039@users.noreply.github.com>
2018-08-12 13:05:58 +0530

    Added console message post deadlock detection.

--- src/org/jtlabs/demo/unittesting/prepare/Deadlock.java
+++ src/org/jtlabs/demo/unittesting/prepare/Deadlock.java
@@ -38,6 +38,19 @@ public class Deadlock {
 
         final DeadlockDetector detector = new DeadlockDetector(new DeadlockConsoleHandler(), 1, ManagementFactory.getThreadMXBean());
         detector.start();
+        
+        // Output
+        // ==================================================================================
+        // Deadlock detected!
+        // "Thread-1" Id=11 BLOCKED on java.lang.Object@54739c9a owned by "Thread-0" Id=10
+        // 	org.jtlabs.demo.unittesting.prepare.Deadlock.lambda$main$1(Deadlock.java:33)
+        // 	org.jtlabs.demo.unittesting.prepare.Deadlock$$Lambda$2/1854731462.run(Unknown Source)
+        // 	java.lang.Thread.run(Thread.java:748)
+        // "Thread-0" Id=10 BLOCKED on java.lang.Object@504c02cc owned by "Thread-1" Id=11
+        // 	org.jtlabs.demo.unittesting.prepare.Deadlock.lambda$main$0(Deadlock.java:21)
+        // 	org.jtlabs.demo.unittesting.prepare.Deadlock$$Lambda$1/1922154895.run(Unknown Source)
+        // ===================================================================================
+        // 	java.lang.Thread.run(Thread.java:748)
     }
 
 }
